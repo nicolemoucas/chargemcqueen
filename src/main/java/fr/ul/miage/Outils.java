111371fdf5b4c545c6411f3e515fe898f0e705d2
Nadege <fraselle.nadege@gmail.com>
2024-05-23 17:27:02 +0200

    Ajout de la méthode pour inscrire les clients dans la classe FormulaireInscriptionClient. Ajout de méthodes pour recevoir des informations sur la plaque d'immatriculation du client. Attente de la base de données. Ajout de détails pour des tests.

--- src/main/java/fr/ul/miage/Outils.java
+++ src/main/java/fr/ul/miage/Outils.java
@@ -2,12 +2,30 @@ package fr.ul.miage;
 
 import java.util.regex.Pattern;
 
+/**
+ * Cette classe est une boite à outils, et contient des méthodes versatiles.
+ */
 public class Outils {
 
     /**
-     * Constante qui contient le pattern d'adresse mail acceptée par l'application.
+     * Constante qui contient le pattern d'adresse mail acceptées par l'application.
      */
     private static final String emailRegex = "^(?=.{1,64}@)[A-Za-z0-9]+([_.-][A-Za-z0-9]+)*@[A-Za-z0-9]+([-.][A-Za-z0-9]+)*\\.[A-Za-z]{2,}$";
+
+    /**
+     * Constante qui contient le pattern de noms acceptés par l'application.
+     */
+    private static final String nomsRegex = "^[A-Za-zÀ-ÿ](?=.{1,29}$)[A-Za-zÀ-ÿ]*((?:\\h+|-)[A-ZÀ-ÿ][A-Za-zÀ-ÿ]*)*$";
+
+    /**
+     * Constante qui contient le pattern de numéros de téléphone acceptés par l'application.
+     */
+    private static final String telephoneRegex = "^(\\+33[1-9][0-9]{8})|(0[1-9][0-9]{8})$";
+
+    /**
+     * Constante qui contient le pattern de numéros de carte bancaire acceptés par l'application.
+     */
+    private static final String regexCarteBancaire = "^[0-9]{15,16}$";
 
     /**
      * Méthode utilisée pour controler que l'adresse mail est valide.
@@ -19,4 +37,38 @@ public class Outils {
                 .matcher(mail)
                 .matches();
     }
+
+    /**
+     * Méthode utilisée pour controler que le nom/prénom du client est valide.
+     * @param nom le nom ou prénom à contrôler.
+     * @return {boolean} true si le nom/prénom est valide, false sinon.
+     */
+    public static boolean verificationNoms(String nom) {
+        return Pattern.compile(nomsRegex)
+                .matcher(nom)
+                .matches();
+    }
+
+    /**
+     * Méthode utilisée pour controler que le numéro de téléphone est valide.
+     * @param numero le numéro de téléphone à contrôler.
+     * @return {boolean} true si le numéro de téléphone est valide, false sinon.
+     */
+    public static boolean verificationTelephone(String numero) {
+        return Pattern.compile(telephoneRegex)
+                .matcher(numero)
+                .matches();
+    }
+
+    /**
+     * Méthode utilisée pour controler que le numéro de carte bancaire est valide.
+     * @param numeroCarte le numéro de carte bancaire à contrôler.
+     * @return {boolean} true si le numéro de carte bancaire est valide, false sinon.
+     */
+    public static boolean verificationCarteBancaire(String numeroCarte) {
+        return Pattern.compile(regexCarteBancaire)
+                .matcher(numeroCarte)
+                .matches();
+    }
+
 }
--- src/test/java/fr/ul/miage/OutilsTests.java
+++ src/test/java/fr/ul/miage/OutilsTests.java
@@ -62,4 +62,87 @@ public class OutilsTests {
         assertFalse(result);
     }
 
+    @ParameterizedTest(name= "Les noms corrects passent.")
+    @CsvSource({
+            "Nadège",
+            "Nicole",
+            "Marie-Clémentine",
+            "Marie Clémentine",
+            "Aña"
+
+    })
+    public void prenomsCorrects(String nom) {
+        boolean result = Outils.verificationNoms(nom);
+        System.out.println(nom);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest(name= "Les noms incorrects ne passent.")
+    @CsvSource({
+            "1234",
+            "N!cole",
+            "@Clémentine",
+            "Marie@Clémentine",
+            "Aña{}"
+
+    })
+    public void prenomsIncorrects(String nom) {
+        boolean result = Outils.verificationNoms(nom);
+        System.out.println(nom);
+        assertFalse(result);
+    }
+
+    @ParameterizedTest(name= "Les numéros de téléphones qui ont le bon format sont acceptés.")
+    @CsvSource({
+            "0643825729",
+            "+33643825729"
+    })
+    public void numeroTelephoneCorrects(String numero) {
+        boolean result = Outils.verificationTelephone(numero);
+        System.out.println(numero);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest(name= "Les numéros de téléphones qui ont le mauvais format ne sont pas acceptés.")
+    @CsvSource({
+            "990643825729",
+            "+336RR124567",
+            "++33387762641"
+    })
+    public void numeroTelephoneIncorrects(String numero) {
+        boolean result = Outils.verificationTelephone(numero);
+        System.out.println(numero);
+        assertFalse(result);
+    }
+
+    @ParameterizedTest(name= "Les numéros de carte qui ont le bon format sont acceptés.")
+    @CsvSource({
+
+            "4970119716321974",
+            "5200828282828210",
+            "371449635398431",
+            "6011014433546101"
+
+})
+    public void numeroCarteBancaireCorrect(String numeroCarte) {
+        boolean result = Outils.verificationCarteBancaire(numeroCarte);
+        System.out.println(numeroCarte);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest(name= "Les numéros de carte qui ont le bon format sont acceptés.")
+    @CsvSource({
+
+            "1974",
+            "vz200zerf2828282",
+            "371449@35-98431",
+            "DROP DATABASE NAME;"
+
+    })
+    public void numeroCarteBancaireIncorrects(String numeroCarte) {
+        boolean result = Outils.verificationCarteBancaire(numeroCarte);
+        System.out.println(numeroCarte);
+        assertFalse(result);
+    }
+
 }
