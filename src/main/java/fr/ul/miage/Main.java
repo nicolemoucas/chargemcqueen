775241453446135dcdd11309c6903982479f2749
nicolemoucas <nicolemoucas@gmail.com>
2024-05-23 11:43:21 +0200

    bienvenue Charge McQueen

--- src/main/java/fr/ul/miage/Main.java
+++ src/main/java/fr/ul/miage/Main.java
@@ -2,6 +2,20 @@ package fr.ul.miage;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        bienvenue();
+        afficherMenu();
+    }
+
+    private static void bienvenue() {
+        System.out.println("Bienvenue chez Charge McQueen !");
+        System.out.println(
+                "           .--------.\n" +
+                "      ____/_____|___ \\___\n" +
+                "     O    _   - |   _   ,*\n" +
+                "      '--(_)-------(_)--'  ");
+    }
+
+    private static void afficherMenu() {
+        System.out.println();
     }
 }
